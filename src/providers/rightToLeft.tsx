58f9ac00a62c63808ba2ede9bc3ea56a8ac609b1
ShlomoNus <shlomonuswork@gmail.com>
2025-11-17 16:47:53 +0000

    refactor: simplify props type definition in RightToLeftProvider component

--- src/providers/rightToLeft.tsx
+++ src/providers/rightToLeft.tsx
@@ -1,5 +1,6 @@
 import { DirectionProvider } from "@radix-ui/react-direction";
+import { type PropsWithChildren } from "react";
 
-export function RightToLeftProvider({ children }: { children: React.ReactNode }) {
+export function RightToLeftProvider({ children }: PropsWithChildren) {
   return <DirectionProvider dir="rtl">{children}</DirectionProvider>;
 }

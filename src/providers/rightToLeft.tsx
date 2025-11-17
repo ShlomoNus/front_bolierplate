83218939ebeb64af3c29bef09c336d0b60df0089
ShlomoNus <shlomonuswork@gmail.com>
2025-11-17 16:43:51 +0000

    feat: add RightToLeftProvider component and update dependencies

---
+++ src/providers/rightToLeft.tsx
@@ -0,0 +1,5 @@
+import { DirectionProvider } from "@radix-ui/react-direction";
+
+export function RightToLeftProvider({ children }: { children: React.ReactNode }) {
+  return <DirectionProvider dir="rtl">{children}</DirectionProvider>;
+}
